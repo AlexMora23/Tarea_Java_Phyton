d23b9a06857e430c7b7b2b7368b258306a52a390
Alex Mora <162t0308@itsm.edu.mx>
2017-05-12 18:40:28 -0500

    Create 4_es_vocal.py
    4- Escribir una función que tome un carácter y devuelva True si es una vocal, de lo
    contrario devuelve False.

---
+++ 4_es_vocal.py
@@ -0,0 +1,23 @@
+def es_vocal():
+    return()
+letra = input("Ingrese una letra>")
+    
+if(letra=='a' or letra=='e' or letra=='i' or letra=='o' or letra=='u'):
+    print(True)
+elif(letra=='A' or letra=='E' or letra=='I' or letra=='O' or letra=='U'):
+    print(True)
+
+else:
+    #if(letra=='e'):
+    print(False)
+  #  else:
+     #   if(letra=='i'):
+    #        print(True)
+      #  else:
+       #     if(letra=='o'):
+        #        print(True)
+         #   else:
+          #      if(letra=='u'):
+           #         print(True)
+            #    else:
+             #       print("False")
